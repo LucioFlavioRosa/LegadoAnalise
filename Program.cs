384b86c93a6fb5558075585921fa29425c732e8e
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-22 12:12:04 -0300

    feat: Program.cs

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Business.Services;
+using Business.DataAccess;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<IEvolucaoAssociadoServices, EvolucaoAssociadoServices>();
+builder.Services.AddScoped<IAssociadosService, AssociadosService>();
+builder.Services.AddScoped<IPremissaService, PremissaService>();
+builder.Services.AddScoped<IDataRepository, DataRepository>();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode();
+
+app.Run();
