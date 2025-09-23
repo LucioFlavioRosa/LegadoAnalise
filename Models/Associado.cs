0b41c1d6fca1a39f64c4d245d688de66ba4cf6bb
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-23 16:00:37 -0300

    refactor: Models/Associado.cs

--- Models/Associado.cs
+++ Models/Associado.cs
@@ -1,42 +1,47 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Peers.Moderno.Models;
 
+[Table("ASSOCIADOS")]
 public class Associado
 {
+    [Key]
+    [Column("IdAssociado")]
     public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public int? IdCargo { get; set; }
-    public int? IdMentor { get; set; }
-    public int? IdPerfil { get; set; }
-    public int? IdVertical { get; set; }
-    public int? IdEmpresa { get; set; }
-    public string Senha { get; set; } = string.Empty;
-    public DateTime? DataAdmissao { get; set; }
-    public bool Ativo { get; set; } = true;
-    public string? FotoBase64 { get; set; }
-    public DateTime DataCriacao { get; set; }
-    public DateTime DataAlteracao { get; set; }
     
-    // Navegação
-    public virtual Cargo? Cargo { get; set; }
-    public virtual Associado? Mentor { get; set; }
-    public virtual Perfil? Perfil { get; set; }
-    public virtual Vertical? Vertical { get; set; }
-    public virtual ICollection<Associado> Mentorados { get; set; } = new List<Associado>();
-    public virtual ICollection<Promocao> Promocoes { get; set; } = new List<Promocao>();
-}
-
-public class AssociadoListaItem
-{
-    public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
     public string Nome { get; set; } = string.Empty;
-    public string NomeCargo { get; set; } = string.Empty;
-    public string NomeMentor { get; set; } = string.Empty;
+    
+    [StringLength(255)]
+    public string? Email { get; set; }
+    
     public bool Ativo { get; set; }
-}
-
-public class DropdownItem
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
+    
+    public bool Socio { get; set; }
+    
+    public int? IdCargo { get; set; }
+    
+    public int? IdMentor { get; set; }
+    
+    public int? IdPerfil { get; set; }
+    
+    public int? IdVertical { get; set; }
+    
+    [ForeignKey("IdCargo")]
+    public virtual Cargo? Cargo { get; set; }
+    
+    [ForeignKey("IdMentor")]
+    public virtual Associado? Mentor { get; set; }
+    
+    public virtual ICollection<Associado> Mentorados { get; set; } = new List<Associado>();
+    
+    [ForeignKey("IdPerfil")]
+    public virtual Perfil? Perfil { get; set; }
+    
+    [ForeignKey("IdVertical")]
+    public virtual Vertical? Vertical { get; set; }
+    
+    public virtual ICollection<Promocao> Promocoes { get; set; } = new List<Promocao>();
 }
