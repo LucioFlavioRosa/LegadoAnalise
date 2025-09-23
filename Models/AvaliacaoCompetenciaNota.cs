4cf48043f9f695a204b0d570458a3300a40e1fa3
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-23 16:40:01 -0300

    refactor: Models/AvaliacaoCompetenciaNota.cs

--- Models/AvaliacaoCompetenciaNota.cs
+++ Models/AvaliacaoCompetenciaNota.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Peers.Moderno.Models;
 
+[Table("AVALIACOESCOMPETENCIASNOTAS")]
 public class AvaliacaoCompetenciaNota
 {
+    [Key]
     public int IdNota { get; set; }
+    
     public string CodigoNota { get; set; } = string.Empty;
-    public bool IndFeedback { get; set; }
-    public int ATV { get; set; }
+    public string? Descricao { get; set; }
+    public decimal? Valor { get; set; }
+    public bool? IndFeedback { get; set; }
+    public bool Ativo { get; set; } = true;
 }
