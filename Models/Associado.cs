a3affb4b8737a9ab5534038f393c05a80046ef71
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-23 14:03:38 -0300

    refactor: Models/Associado.cs

--- Models/Associado.cs
+++ Models/Associado.cs
@@ -1,54 +1,29 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Peers.Moderno.Models;
 
-[Table("ASSOCIADOS")]
 public class Associado
 {
-    [Key]
     public int Id { get; set; }
-    
-    [Required]
-    [StringLength(255)]
     public string Nome { get; set; } = string.Empty;
-    
-    [Required]
-    [StringLength(255)]
     public string Email { get; set; } = string.Empty;
-    
-    [Required]
-    [StringLength(255)]
+    public int? IdCargo { get; set; }
+    public int? IdMentor { get; set; }
+    public int? IdPerfil { get; set; }
+    public int? IdVertical { get; set; }
+    public int? IdEmpresa { get; set; }
     public string Senha { get; set; } = string.Empty;
-    
-    public int IdCargo { get; set; }
-    public int IdMentor { get; set; }
-    public int IdPerfil { get; set; }
-    public int IdVertical { get; set; }
-    
     public DateTime? DataAdmissao { get; set; }
     public bool Ativo { get; set; } = true;
-    
     public string? FotoBase64 { get; set; }
-    
     public DateTime DataCriacao { get; set; }
     public DateTime DataAlteracao { get; set; }
     
     // Navegação
-    [ForeignKey("IdCargo")]
     public virtual Cargo? Cargo { get; set; }
-    
-    [ForeignKey("IdMentor")]
     public virtual Associado? Mentor { get; set; }
-    
-    [ForeignKey("IdPerfil")]
     public virtual Perfil? Perfil { get; set; }
-    
-    [ForeignKey("IdVertical")]
     public virtual Vertical? Vertical { get; set; }
-    
-    public virtual ICollection<Promocao> Promocoes { get; set; } = new List<Promocao>();
     public virtual ICollection<Associado> Mentorados { get; set; } = new List<Associado>();
+    public virtual ICollection<Promocao> Promocoes { get; set; } = new List<Promocao>();
 }
 
 public class AssociadoListaItem
