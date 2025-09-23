400a9e2f82d2706d39ff5ed6be64a594b6ea77b5
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-23 14:38:10 -0300

    delete

--- Business/Model/ModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Business.Model
-{
-    public class ModelBase
-    {
-        public int Id { get; set; }
-    }
-}
--- Business/Model/PessoaModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Business.Model
-{
-    public class PessoaModel : ModelBase
-    {
-        public string Nome { get; set; }
-        public CargoModel Cargo { get; set; }
-        public PessoaModel Mentor { get; set; }
-    }
-}
