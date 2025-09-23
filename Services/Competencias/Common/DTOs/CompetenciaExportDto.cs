0052726d498785bb38a51e5158c0062ed9249dba
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-23 16:38:04 -0300

    feat: Services/Competencias/Common/DTOs/CompetenciaExportDto.cs

---
+++ Services/Competencias/Common/DTOs/CompetenciaExportDto.cs
@@ -0,0 +1,20 @@
+namespace Peers.Moderno.Services.Competencias.Common.DTOs;
+
+public class CompetenciaExportDto
+{
+    public int IdCompetencia { get; set; }
+    public int IdCargo { get; set; }
+    public string Cargo { get; set; } = string.Empty;
+    public int IdEixo { get; set; }
+    public string Eixo { get; set; } = string.Empty;
+    public int IdSubCompetencia { get; set; }
+    public string SubCompetencia { get; set; } = string.Empty;
+    public int IdDimensao { get; set; }
+    public string Dimensao { get; set; } = string.Empty;
+    public string DetalheNivelAtual { get; set; } = string.Empty;
+    public string CompetenciaAtual { get; set; } = string.Empty;
+    public string PalavrasChave { get; set; } = string.Empty;
+    public string TipoAvaliacao { get; set; } = string.Empty;
+    public string Escopo { get; set; } = string.Empty;
+    public int ATV { get; set; }
+}
