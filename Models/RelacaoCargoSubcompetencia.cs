68328e5c5be8cf6fbfd9c2a282f8c388318d68cd
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-23 16:51:22 -0300

    refactor: Models/RelacaoCargoSubcompetencia.cs

--- Models/RelacaoCargoSubcompetencia.cs
+++ Models/RelacaoCargoSubcompetencia.cs
@@ -1,13 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Peers.Moderno.Models;
 
+[Table("RELACAO_CARGO_SUBCOMPETENCIA")]
 public class RelacaoCargoSubcompetencia
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Column("idCargo")]
     public int IdCargo { get; set; }
+    
+    [Column("idSubcompetencia")]
     public int IdSubcompetencia { get; set; }
+    
     public string Descricao { get; set; } = string.Empty;
     
-    // Navegação
+    // Navigation Properties
     public virtual Cargo? Cargo { get; set; }
     public virtual SubCompetencia? SubCompetencia { get; set; }
 }
