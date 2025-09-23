332558fa4c5c21e9c08a032d6d4e29bd04e5314f
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-23 16:40:02 -0300

    refactor: Models/ModoCalculoCompetencia.cs

--- Models/ModoCalculoCompetencia.cs
+++ Models/ModoCalculoCompetencia.cs
@@ -1,7 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Peers.Moderno.Models;
 
+[Table("MODOSCALCULOSCOMPETENCIAS")]
 public class ModoCalculoCompetencia
 {
+    [Key]
+    [Column("idModo")]
     public int IdModo { get; set; }
+    
     public string ModoDesc { get; set; } = string.Empty;
+    public string? Observacoes { get; set; }
+    public bool Ativo { get; set; } = true;
 }
