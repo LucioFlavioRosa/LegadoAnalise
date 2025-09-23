671c7dad49ea468b161c2a6d95feb67ddcfa8e6f
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-23 15:37:05 -0300

    refactor: wwwroot/js/site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,14 @@
-window.downloadFile = (fileName, base64Data) => {
+// Função para download de arquivos
+window.downloadFile = (filename, base64Data) => {
     const link = document.createElement('a');
     link.href = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,' + base64Data;
-    link.download = fileName;
+    link.download = filename;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+};
+
+// Função para confirmar ações
+window.confirm = (message) => {
+    return confirm(message);
 };
