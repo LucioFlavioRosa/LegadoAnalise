b9c05a6162f338222fe28d7542a9babadaf1ade0
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-23 15:36:48 -0300

    refactor: Models/Cargo.cs

--- Models/Cargo.cs
+++ Models/Cargo.cs
@@ -4,9 +4,16 @@ public class Cargo
 {
     public int IdCargo { get; set; }
     public string Nome { get; set; } = string.Empty;
-    public int ATV { get; set; }
+    public int? IdProximoCargo { get; set; }
+    public int TempoMinimoPromocao { get; set; } = 12;
+    public string? Funcao { get; set; }
+    public string? Autonomia { get; set; }
+    public string? EscopoDeAtuacao { get; set; }
+    public string? NivelInterlocucao { get; set; }
+    public int ATV { get; set; } = 1;
+    public DateTime DHC { get; set; } = DateTime.Now;
     
-    // Navegação
+    // Navigation properties
+    public virtual Cargo? ProximoCargo { get; set; }
     public virtual ICollection<Competencia> Competencias { get; set; } = new List<Competencia>();
-    public virtual ICollection<RelacaoCargoSubcompetencia> RelacoesCargo { get; set; } = new List<RelacaoCargoSubcompetencia>();
 }
