768b1c6cdb05700252719ffd80d0ed983d656047
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-24 13:07:23 -0300

    feat: Models/PerguntaEncerramento.cs

---
+++ Models/PerguntaEncerramento.cs
@@ -0,0 +1,12 @@
+namespace Peers.Moderno.Models;
+
+public class PerguntaEncerramento
+{
+    public int Id { get; set; }
+    public string Codigo { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public bool PossuiComentario { get; set; }
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+    public DateTime? DataAtualizacao { get; set; }
+    public bool Ativo { get; set; } = true;
+}
