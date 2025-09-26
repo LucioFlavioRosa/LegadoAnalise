3cfc3a96f68bd032520a5581bddc3c93885d58c7
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-26 11:47:30 -0300

    refactor: Services/PDI/Common/IPDIService.cs

--- Services/PDI/Common/IPDIService.cs
+++ Services/PDI/Common/IPDIService.cs
@@ -4,8 +4,9 @@ namespace Services.PDI.Common;
 
 public interface IPDIService
 {
-    Task<List<PDIPeriodosModel>> ObterPeriodosAsync(int idAssociado);
-    Task<List<PDIQuestoesModel>> ObterQuestoesAsync(int idPeriodo);
-    Task<List<PDIRespostasModel>> ObterRespostasAsync(int idAssociado, int idPeriodo);
+    Task<List<PDIPeriodoModel>> GetPeriodosAsync(int idAssociado);
+    Task<List<PDIColunaModel>> GetColunasAsync(int idAssociado, int idPeriodo);
+    Task<List<PDIRespostaModel>> GetRespostasAsync(int idAssociado, int idPeriodo);
     Task AtualizarRespostaAsync(int idPDIResposta, string resposta);
+    Task ValidaPDIRespostasAsync(int idAssociado, int idPeriodo);
 }
