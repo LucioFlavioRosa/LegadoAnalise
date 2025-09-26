1d3bc82f8b84e21f66ae173283148b596f35b457
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-26 11:36:14 -0300

    feat: Services/PDI/Common/Models/PDIPillsModel.cs

---
+++ Services/PDI/Common/Models/PDIPillsModel.cs
@@ -0,0 +1,11 @@
+namespace Services.PDI.Common.Models;
+
+public class PDIPillsModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string Href { get; set; } = string.Empty;
+    public string Ariacontrols { get; set; } = string.Empty;
+    public string Ariaselected { get; set; } = string.Empty;
+    public string Active { get; set; } = string.Empty;
+    public string Periodo { get; set; } = string.Empty;
+}
