bcf6ae12496c4e0883eb3aadb33c57d46bad0916
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-26 10:57:34 -0300

    feat: Services/FrentesInternas/Common/Models/PDIPillsModel.cs

---
+++ Services/FrentesInternas/Common/Models/PDIPillsModel.cs
@@ -0,0 +1,12 @@
+namespace Services.FrentesInternas.Common.Models;
+
+public class PDIPillsModel
+{
+    public string id { get; set; } = string.Empty;
+    public string href { get; set; } = string.Empty;
+    public string ariacontrols { get; set; } = string.Empty;
+    public string ariaselected { get; set; } = string.Empty;
+    public string active { get; set; } = string.Empty;
+    public string classe { get; set; } = string.Empty;
+    public string Periodo { get; set; } = string.Empty;
+}
