de56d4d4c2e9294b16bf1a2585899bf24a651f57
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-29 13:11:41 -0300

    refactor: Tests/Services/Common/TelemetryServiceTests.cs

--- Tests/Services/Common/TelemetryServiceTests.cs
+++ Tests/Services/Common/TelemetryServiceTests.cs
@@ -10,172 +10,114 @@ namespace Tests.Services.Common
     public class TelemetryServiceTests
     {
         private readonly Mock<TelemetryClient> _mockTelemetryClient;
-        private readonly TelemetryService _telemetryService;
+        private readonly TelemetryService _service;
 
         public TelemetryServiceTests()
         {
             _mockTelemetryClient = new Mock<TelemetryClient>();
-            _telemetryService = new TelemetryService(_mockTelemetryClient.Object);
+            _service = new TelemetryService(_mockTelemetryClient.Object);
         }
 
         [Fact]
-        public void TrackEvent_ChamaTelemetryClientCorretamente()
+        public void TrackEvent_DeveChamarTelemetryClient()
         {
             var eventName = "TestEvent";
             var properties = new Dictionary<string, string> { { "key1", "value1" } };
-            var metrics = new Dictionary<string, double> { { "metric1", 1.5 } };
+            var metrics = new Dictionary<string, double> { { "metric1", 1.0 } };
 
-            _telemetryService.TrackEvent(eventName, properties, metrics);
+            _service.TrackEvent(eventName, properties, metrics);
 
-            _mockTelemetryClient.Verify(t => t.TrackEvent(eventName, properties, metrics), Times.Once);
+            _mockTelemetryClient.Verify(x => x.TrackEvent(eventName, properties, metrics), Times.Once);
         }
 
         [Fact]
-        public void TrackEvent_ComParametrosNulos_ChamaTelemetryClientCorretamente()
+        public void TrackEvent_SemParametrosOpcionais_DeveChamarComNulls()
         {
-            var eventName = "TestEvent";
+            var eventName = "SimpleEvent";
 
-            _telemetryService.TrackEvent(eventName, null, null);
+            _service.TrackEvent(eventName);
 
-            _mockTelemetryClient.Verify(t => t.TrackEvent(eventName, null, null), Times.Once);
+            _mockTelemetryClient.Verify(x => x.TrackEvent(eventName, null, null), Times.Once);
         }
 
         [Fact]
-        public void TrackEvent_SemParametrosOpcionais_ChamaTelemetryClientCorretamente()
-        {
-            var eventName = "TestEvent";
-
-            _telemetryService.TrackEvent(eventName);
-
-            _mockTelemetryClient.Verify(t => t.TrackEvent(eventName, null, null), Times.Once);
-        }
-
-        [Fact]
-        public void TrackException_ChamaTelemetryClientCorretamente()
+        public void TrackException_DeveChamarTelemetryClient()
         {
             var exception = new Exception("Test exception");
             var properties = new Dictionary<string, string> { { "context", "test" } };
 
-            _telemetryService.TrackException(exception, properties);
+            _service.TrackException(exception, properties);
 
-            _mockTelemetryClient.Verify(t => t.TrackException(exception, properties), Times.Once);
+            _mockTelemetryClient.Verify(x => x.TrackException(exception, properties), Times.Once);
         }
 
         [Fact]
-        public void TrackException_ComPropertiesNulas_ChamaTelemetryClientCorretamente()
+        public void TrackException_SemProperties_DeveChamarComNull()
         {
-            var exception = new Exception("Test exception");
+            var exception = new Exception("Simple exception");
 
-            _telemetryService.TrackException(exception, null);
+            _service.TrackException(exception);
 
-            _mockTelemetryClient.Verify(t => t.TrackException(exception, null), Times.Once);
+            _mockTelemetryClient.Verify(x => x.TrackException(exception, null), Times.Once);
         }
 
         [Fact]
-        public void TrackException_SemParametrosOpcionais_ChamaTelemetryClientCorretamente()
+        public void TrackDependency_DeveChamarTelemetryClient()
         {
-            var exception = new Exception("Test exception");
-
-            _telemetryService.TrackException(exception);
-
-            _mockTelemetryClient.Verify(t => t.TrackException(exception, null), Times.Once);
-        }
-
-        [Fact]
-        public void TrackDependency_ChamaTelemetryClientCorretamente()
-        {
-            var dependencyTypeName = "SQL";
-            var dependencyName = "GetUsers";
-            var data = "SELECT * FROM Users";
+            var dependencyTypeName = "HTTP";
+            var dependencyName = "API Call";
+            var data = "GET /api/test";
             var startTime = DateTimeOffset.Now;
             var duration = TimeSpan.FromMilliseconds(500);
             var success = true;
 
-            _telemetryService.TrackDependency(dependencyTypeName, dependencyName, data, startTime, duration, success);
+            _service.TrackDependency(dependencyTypeName, dependencyName, data, startTime, duration, success);
 
-            _mockTelemetryClient.Verify(t => t.TrackDependency(
-                dependencyTypeName, dependencyName, data, startTime, duration, success), Times.Once);
+            _mockTelemetryClient.Verify(x => x.TrackDependency(dependencyTypeName, dependencyName, data, startTime, duration, success), Times.Once);
         }
 
         [Fact]
-        public void TrackPageView_ChamaTelemetryClientCorretamente()
+        public void TrackPageView_DeveChamarTelemetryClient()
         {
-            var pageName = "HomePage";
-            var properties = new Dictionary<string, string> { { "user", "testuser" } };
+            var pageName = "TestPage";
+            var properties = new Dictionary<string, string> { { "userId", "123" } };
 
-            _telemetryService.TrackPageView(pageName, properties);
+            _service.TrackPageView(pageName, properties);
 
-            _mockTelemetryClient.Verify(t => t.TrackPageView(pageName, properties), Times.Once);
+            _mockTelemetryClient.Verify(x => x.TrackPageView(pageName, properties), Times.Once);
         }
 
         [Fact]
-        public void TrackPageView_ComPropertiesNulas_ChamaTelemetryClientCorretamente()
+        public void TrackPageView_SemProperties_DeveChamarComNull()
         {
-            var pageName = "HomePage";
+            var pageName = "SimplePage";
 
-            _telemetryService.TrackPageView(pageName, null);
+            _service.TrackPageView(pageName);
 
-            _mockTelemetryClient.Verify(t => t.TrackPageView(pageName, null), Times.Once);
+            _mockTelemetryClient.Verify(x => x.TrackPageView(pageName, null), Times.Once);
         }
 
         [Fact]
-        public void TrackPageView_SemParametrosOpcionais_ChamaTelemetryClientCorretamente()
-        {
-            var pageName = "HomePage";
-
-            _telemetryService.TrackPageView(pageName);
-
-            _mockTelemetryClient.Verify(t => t.TrackPageView(pageName, null), Times.Once);
-        }
-
-        [Fact]
-        public void TrackMetric_ChamaTelemetryClientCorretamente()
+        public void TrackMetric_DeveChamarTelemetryClient()
         {
             var metricName = "ResponseTime";
             var value = 250.5;
-            var properties = new Dictionary<string, string> { { "endpoint", "/api/users" } };
+            var properties = new Dictionary<string, string> { { "endpoint", "/api/test" } };
 
-            _telemetryService.TrackMetric(metricName, value, properties);
+            _service.TrackMetric(metricName, value, properties);
 
-            _mockTelemetryClient.Verify(t => t.TrackMetric(metricName, value, properties), Times.Once);
+            _mockTelemetryClient.Verify(x => x.TrackMetric(metricName, value, properties), Times.Once);
         }
 
         [Fact]
-        public void TrackMetric_ComPropertiesNulas_ChamaTelemetryClientCorretamente()
+        public void TrackMetric_SemProperties_DeveChamarComNull()
         {
-            var metricName = "ResponseTime";
-            var value = 250.5;
+            var metricName = "SimpleMetric";
+            var value = 100.0;
 
-            _telemetryService.TrackMetric(metricName, value, null);
+            _service.TrackMetric(metricName, value);
 
-            _mockTelemetryClient.Verify(t => t.TrackMetric(metricName, value, null), Times.Once);
-        }
-
-        [Fact]
-        public void TrackMetric_SemParametrosOpcionais_ChamaTelemetryClientCorretamente()
-        {
-            var metricName = "ResponseTime";
-            var value = 250.5;
-
-            _telemetryService.TrackMetric(metricName, value);
-
-            _mockTelemetryClient.Verify(t => t.TrackMetric(metricName, value, null), Times.Once);
-        }
-
-        [Fact]
-        public void MultipleOperations_ChamaTelemetryClientMultiplasVezes()
-        {
-            var eventName = "Event1";
-            var exception = new Exception("Error1");
-            var pageName = "Page1";
-
-            _telemetryService.TrackEvent(eventName);
-            _telemetryService.TrackException(exception);
-            _telemetryService.TrackPageView(pageName);
-
-            _mockTelemetryClient.Verify(t => t.TrackEvent(eventName, null, null), Times.Once);
-            _mockTelemetryClient.Verify(t => t.TrackException(exception, null), Times.Once);
-            _mockTelemetryClient.Verify(t => t.TrackPageView(pageName, null), Times.Once);
+            _mockTelemetryClient.Verify(x => x.TrackMetric(metricName, value, null), Times.Once);
         }
     }
 }
