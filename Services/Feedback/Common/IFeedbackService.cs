d622909b8813202fdfcf66f15efe258517c8d95e
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-29 15:18:08 -0300

    refactor: Services/Feedback/Common/IFeedbackService.cs

--- Services/Feedback/Common/IFeedbackService.cs
+++ Services/Feedback/Common/IFeedbackService.cs
@@ -2,7 +2,7 @@ namespace Peers.Moderno.Services.Feedback.Common;
 
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using Peers.Moderno.Services.Feedback.Common;
+using Peers.Moderno.Models;
 
 public interface IFeedbackService
 {
@@ -21,60 +21,4 @@ public interface IFeedbackService
     Task AvancarProximaEtapaPerformanceAsync(AvaliacaoPerformanceModel avaliacao, FeedbackAvaliacaoEmailModel avaliacaoEmail, string tipoAvaliacao);
     Task AvancarProximaEtapaCompetenciaAsync(AvaliacaoCompetenciaModel avaliacao, FeedbackAvaliacaoEmailModel avaliacaoEmail, string tipoAvaliacao);
     Task AvancarProximaEtapaEmailAsync(FeedbackAvaliacaoEmailModel avaliacaoEmail);
-}
-
-public class ProjetoFeedbackModel
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-}
-
-public class ClienteFeedbackModel
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-}
-
-public class PeriodoFeedbackModel
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-}
-
-public class StatusFeedbackModel
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-}
-
-public class FeedbackFiltroModel
-{
-    public int? ProjetoId { get; set; }
-    public int? StatusId { get; set; }
-    public int? PeriodoId { get; set; }
-    public int? ClienteId { get; set; }
-}
-
-public class FeedbackAvaliacaoEmailModel
-{
-    public int Id { get; set; }
-    public string Email { get; set; } = string.Empty;
-}
-
-public class AvaliacaoPerformanceModel
-{
-    public int Id { get; set; }
-    public string Descricao { get; set; } = string.Empty;
-}
-
-public class AvaliacaoCompetenciaModel
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-}
-
-public class AssociadoFeedbackModel
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
 }
