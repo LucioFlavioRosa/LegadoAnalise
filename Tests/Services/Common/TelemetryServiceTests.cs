b2f79054017bae5ba2067ef479e2aa333bc6a551
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-29 13:24:47 -0300

    refactor: Tests/Services/Common/TelemetryServiceTests.cs

--- Tests/Services/Common/TelemetryServiceTests.cs
+++ Tests/Services/Common/TelemetryServiceTests.cs
@@ -19,51 +19,64 @@ namespace Tests.Services.Common
         }
 
         [Fact]
-        public void TrackEvent_DeveChamarTelemetryClient()
+        public void TrackEvent_DevePassarParametrosCorretamente()
         {
+            // Arrange
             var eventName = "TestEvent";
             var properties = new Dictionary<string, string> { { "key1", "value1" } };
-            var metrics = new Dictionary<string, double> { { "metric1", 1.0 } };
+            var metrics = new Dictionary<string, double> { { "metric1", 1.5 } };
 
+            // Act
             _service.TrackEvent(eventName, properties, metrics);
 
+            // Assert
             _mockTelemetryClient.Verify(x => x.TrackEvent(eventName, properties, metrics), Times.Once);
         }
 
         [Fact]
-        public void TrackEvent_SemParametrosOpcionais_DeveChamarComNulls()
+        public void TrackEvent_ComParametrosNulos_DevePassarNulos()
         {
-            var eventName = "SimpleEvent";
+            // Arrange
+            var eventName = "TestEvent";
 
-            _service.TrackEvent(eventName);
+            // Act
+            _service.TrackEvent(eventName, null, null);
 
+            // Assert
             _mockTelemetryClient.Verify(x => x.TrackEvent(eventName, null, null), Times.Once);
         }
 
         [Fact]
-        public void TrackException_DeveChamarTelemetryClient()
+        public void TrackException_DevePassarParametrosCorretamente()
         {
+            // Arrange
             var exception = new Exception("Test exception");
-            var properties = new Dictionary<string, string> { { "context", "test" } };
+            var properties = new Dictionary<string, string> { { "key1", "value1" } };
 
+            // Act
             _service.TrackException(exception, properties);
 
+            // Assert
             _mockTelemetryClient.Verify(x => x.TrackException(exception, properties), Times.Once);
         }
 
         [Fact]
-        public void TrackException_SemProperties_DeveChamarComNull()
+        public void TrackException_ComPropertiesNulas_DevePassarNulo()
         {
-            var exception = new Exception("Simple exception");
+            // Arrange
+            var exception = new Exception("Test exception");
 
-            _service.TrackException(exception);
+            // Act
+            _service.TrackException(exception, null);
 
+            // Assert
             _mockTelemetryClient.Verify(x => x.TrackException(exception, null), Times.Once);
         }
 
         [Fact]
-        public void TrackDependency_DeveChamarTelemetryClient()
+        public void TrackDependency_DevePassarParametrosCorretamente()
         {
+            // Arrange
             var dependencyTypeName = "HTTP";
             var dependencyName = "API Call";
             var data = "GET /api/test";
@@ -71,52 +84,66 @@ namespace Tests.Services.Common
             var duration = TimeSpan.FromMilliseconds(500);
             var success = true;
 
+            // Act
             _service.TrackDependency(dependencyTypeName, dependencyName, data, startTime, duration, success);
 
+            // Assert
             _mockTelemetryClient.Verify(x => x.TrackDependency(dependencyTypeName, dependencyName, data, startTime, duration, success), Times.Once);
         }
 
         [Fact]
-        public void TrackPageView_DeveChamarTelemetryClient()
+        public void TrackPageView_DevePassarParametrosCorretamente()
         {
+            // Arrange
             var pageName = "TestPage";
             var properties = new Dictionary<string, string> { { "userId", "123" } };
 
+            // Act
             _service.TrackPageView(pageName, properties);
 
+            // Assert
             _mockTelemetryClient.Verify(x => x.TrackPageView(pageName, properties), Times.Once);
         }
 
         [Fact]
-        public void TrackPageView_SemProperties_DeveChamarComNull()
+        public void TrackPageView_ComPropertiesNulas_DevePassarNulo()
         {
-            var pageName = "SimplePage";
+            // Arrange
+            var pageName = "TestPage";
 
-            _service.TrackPageView(pageName);
+            // Act
+            _service.TrackPageView(pageName, null);
 
+            // Assert
             _mockTelemetryClient.Verify(x => x.TrackPageView(pageName, null), Times.Once);
         }
 
         [Fact]
-        public void TrackMetric_DeveChamarTelemetryClient()
+        public void TrackMetric_DevePassarParametrosCorretamente()
         {
+            // Arrange
             var metricName = "ResponseTime";
             var value = 250.5;
             var properties = new Dictionary<string, string> { { "endpoint", "/api/test" } };
 
+            // Act
             _service.TrackMetric(metricName, value, properties);
 
+            // Assert
             _mockTelemetryClient.Verify(x => x.TrackMetric(metricName, value, properties), Times.Once);
         }
 
         [Fact]
-        public void TrackMetric_SemProperties_DeveChamarComNull()
+        public void TrackMetric_ComPropertiesNulas_DevePassarNulo()
         {
-            var metricName = "SimpleMetric";
-            var value = 100.0;
+            // Arrange
+            var metricName = "ResponseTime";
+            var value = 250.5;
 
-            _service.TrackMetric(metricName, value);
+            // Act
+            _service.TrackMetric(metricName, value, null);
 
+            // Assert
             _mockTelemetryClient.Verify(x => x.TrackMetric(metricName, value, null), Times.Once);
         }
     }
