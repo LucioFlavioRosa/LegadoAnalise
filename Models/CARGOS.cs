cf9ce9ba3e816a331221db7963f29cf3ef0b485d
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-01 16:40:25 -0300

    refactor: Models/CARGOS.cs

--- Models/CARGOS.cs
+++ Models/CARGOS.cs
@@ -1,19 +1,16 @@
-using System;
-using System.Collections.Generic;
-
-namespace Peers.Moderno.Models
+namespace Models
 {
     public class CARGOS
     {
         public int IdCargo { get; set; }
-        public string Cargo { get; set; } = string.Empty;
+        public string Cargo { get; set; }
+        public int? idProximoCargo { get; set; }
+        public int TempoMinimoPromocao { get; set; }
+        public string Funcao { get; set; }
+        public string Autonomia { get; set; }
+        public string EscopoDeAtuacao { get; set; }
+        public string NivelInterlocucao { get; set; }
         public int ATV { get; set; }
         public DateTime DHC { get; set; }
-        public int USR { get; set; }
-
-        // Navegação
-        public ICollection<ASSOCIADOS>? ASSOCIADOS { get; set; }
-        public ICollection<PROMOCOES>? PROMOCOES_ANTIGO { get; set; } // Cargo antigo
-        public ICollection<PROMOCOES>? PROMOCOES_NOVO { get; set; } // Cargo novo
     }
 }
