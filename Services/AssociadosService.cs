e792ec2c62d9c9c59a219733bc529b7796597a89
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-02 11:30:27 -0300

    refactor: Services/AssociadosService.cs

--- Services/AssociadosService.cs
+++ Services/AssociadosService.cs
@@ -2,8 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Peers.Moderno.Models;
 using Peers.Moderno.Data;
+using Peers.Moderno.Models;
 
 namespace Peers.Moderno.Services
 {
@@ -17,22 +17,28 @@ namespace Peers.Moderno.Services
 
         public async Task<List<ASSOCIADOS>> ObterAssociadosAsync()
         {
-            return await _context.Associados.Include(a => a.CARGOS).Include(a => a.PERFIS).Include(a => a.EMPRESAS).Include(a => a.ASSOCIADOS2).ToListAsync();
+            return await _context.Associados
+                .Include(a => a.CARGOS)
+                .Include(a => a.PERFIS)
+                .Include(a => a.EMPRESAS)
+                .Include(a => a.ASSOCIADOS2)
+                .ToListAsync();
         }
 
         public async Task<ASSOCIADOS> ObterAssociadoAsync(int id)
         {
-            return await _context.Associados.Include(a => a.CARGOS).Include(a => a.PERFIS).Include(a => a.EMPRESAS).Include(a => a.ASSOCIADOS2).FirstOrDefaultAsync(a => a.IdAssociado == id);
+            return await _context.Associados
+                .Include(a => a.CARGOS)
+                .Include(a => a.PERFIS)
+                .Include(a => a.EMPRESAS)
+                .Include(a => a.ASSOCIADOS2)
+                .FirstOrDefaultAsync(a => a.IdAssociado == id);
         }
 
         public async Task<ASSOCIADOS> ObterAssociadoPorEmailAsync(string email)
         {
-            return await _context.Associados.FirstOrDefaultAsync(a => a.Email == email);
-        }
-
-        public async Task<ASSOCIADOS> ObterUltimoAssociadoAsync()
-        {
-            return await _context.Associados.OrderByDescending(a => a.IdAssociado).FirstOrDefaultAsync();
+            return await _context.Associados
+                .FirstOrDefaultAsync(a => a.Email == email);
         }
 
         public async Task<bool> InserirAssociadoAsync(ASSOCIADOS associado)
@@ -57,6 +63,11 @@ namespace Peers.Moderno.Services
             existente.IdStatus = 0;
             _context.Associados.Update(existente);
             return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<ASSOCIADOS> ObterUltimoAssociadoAsync()
+        {
+            return await _context.Associados.OrderByDescending(a => a.IdAssociado).FirstOrDefaultAsync();
         }
     }
 }
