e0821e877852cafe1cb50b0fc82f082e0b2d2806
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-02 11:30:32 -0300

    refactor: Services/CargosService.cs

--- Services/CargosService.cs
+++ Services/CargosService.cs
@@ -2,8 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Peers.Moderno.Models;
 using Peers.Moderno.Data;
+using Peers.Moderno.Models;
 
 namespace Peers.Moderno.Services
 {
@@ -17,18 +17,24 @@ namespace Peers.Moderno.Services
 
         public async Task<List<CARGOS>> ObterListaCargosAsync(bool ativo)
         {
-            return await _context.Cargos.Where(c => !ativo || c.ATV == 1).ToListAsync();
-        }
-
-        public async Task<List<PROMOCOES>> ObterTodasPromocoesAssociadoAsync(int idAssociado)
-        {
-            return await _context.Promocoes.Where(p => p.idAssociado == idAssociado).Include(p => p.CARGOS).Include(p => p.CARGOS1).ToListAsync();
+            return await _context.Cargos
+                .Where(c => !ativo || c.ATV == 1)
+                .ToListAsync();
         }
 
         public async Task<bool> AdicionarPromocaoAsync(PROMOCOES promocao)
         {
             _context.Promocoes.Add(promocao);
             return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<List<PROMOCOES>> ObterTodasPromocoesAssociadoAsync(int idAssociado)
+        {
+            return await _context.Promocoes
+                .Where(p => p.idAssociado == idAssociado)
+                .Include(p => p.CARGOS)
+                .Include(p => p.CARGOS1)
+                .ToListAsync();
         }
 
         public async Task<bool> AlterarPromocaoComentarioAsync(int idPromocao, string comentario)
@@ -42,12 +48,19 @@ namespace Peers.Moderno.Services
 
         public async Task<List<PROMOCOES>> ObterListaPromocoesAsync()
         {
-            return await _context.Promocoes.Include(p => p.ASSOCIADOS).Include(p => p.CARGOS).Include(p => p.CARGOS1).ToListAsync();
+            return await _context.Promocoes
+                .Include(p => p.ASSOCIADOS)
+                .Include(p => p.CARGOS)
+                .Include(p => p.CARGOS1)
+                .ToListAsync();
         }
 
         public async Task<PROMOCOES> ObterPromocaoAsync(int idAssociado, int idCargoAnterior, int idCargoNovo)
         {
-            return await _context.Promocoes.FirstOrDefaultAsync(p => p.idAssociado == idAssociado && p.idCargoAnterior == idCargoAnterior && p.idCargoNovo == idCargoNovo);
+            return await _context.Promocoes.FirstOrDefaultAsync(p =>
+                p.idAssociado == idAssociado &&
+                p.idCargoAnterior == idCargoAnterior &&
+                p.idCargoNovo == idCargoNovo);
         }
 
         public async Task<bool> AlterarPromocaoAsync(PROMOCOES promocao)
