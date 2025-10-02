afc2753f8287db014c889d0500a2c7c4c79f1503
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-02 11:30:31 -0300

    refactor: Services/ICargosService.cs

--- Services/ICargosService.cs
+++ Services/ICargosService.cs
@@ -7,8 +7,8 @@ namespace Peers.Moderno.Services
     public interface ICargosService
     {
         Task<List<CARGOS>> ObterListaCargosAsync(bool ativo);
-        Task<List<PROMOCOES>> ObterTodasPromocoesAssociadoAsync(int idAssociado);
         Task<bool> AdicionarPromocaoAsync(PROMOCOES promocao);
+        Task<List<PROMOCOES>> ObterTodasPromocoesAssociadoAsync(int idAssociado);
         Task<bool> AlterarPromocaoComentarioAsync(int idPromocao, string comentario);
         Task<List<PROMOCOES>> ObterListaPromocoesAsync();
         Task<PROMOCOES> ObterPromocaoAsync(int idAssociado, int idCargoAnterior, int idCargoNovo);
