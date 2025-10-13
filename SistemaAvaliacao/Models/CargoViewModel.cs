d4febac95dd8ddb47cb07d74ed60075a37b476cf
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-13 11:17:53 -0300

    refactor: SistemaAvaliacao/Models/CargoViewModel.cs

--- SistemaAvaliacao/Models/CargoViewModel.cs
+++ SistemaAvaliacao/Models/CargoViewModel.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace SistemaAvaliacao.Models
 {
     public class CargoViewModel
@@ -7,20 +5,12 @@ namespace SistemaAvaliacao.Models
         public int IdCargo { get; set; }
         public string Cargo { get; set; }
         public int? ProximoCargoId { get; set; }
-        public string ProximoCargoNome { get; set; }
-        public int TempoMinimoMeses { get; set; }
+        public string ProximoCargo { get; set; }
+        public int TempoMinimoPromocaoMeses { get; set; }
         public string Funcao { get; set; }
         public string Autonomia { get; set; }
         public string EscopoAtuacao { get; set; }
         public string NivelInterlocucao { get; set; }
         public bool Ativo { get; set; }
-        public IEnumerable<CargoViewModel> ListaCargos { get; set; }
-        public IEnumerable<StatusCargoItem> StatusOptions { get; set; }
-    }
-
-    public class StatusCargoItem
-    {
-        public int Value { get; set; }
-        public string Text { get; set; }
     }
 }
